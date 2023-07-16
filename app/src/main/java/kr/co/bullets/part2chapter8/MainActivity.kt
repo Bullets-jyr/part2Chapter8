aa7d5d730f0a732f79f4c6ca28c225c75013aa86
Bullets-jyr <jyr@bullets.co.kr>
2023-07-16 17:29:46 +0900

    03. 네이버지도 사용해보기

--- app/src/main/java/kr/co/bullets/part2chapter8/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part2chapter8/MainActivity.kt
@@ -2,10 +2,72 @@ package kr.co.bullets.part2chapter8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import kr.co.bullets.part2chapter8.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var naverMap: NaverMap
+    private var isMapInit = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onMapReady(mapObject: NaverMap) {
+        naverMap = mapObject
+        isMapInit = true
+
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.3666102, 126.9783881))
+            .animate(CameraAnimation.Easing, 2000)
+            .reason(1000)
+
+        naverMap.moveCamera(cameraUpdate)
     }
 }
